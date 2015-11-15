9e11defb6521ea7e5b307faf94cfcd77a2de956b
hirschenberger <falco.hirschenberger@gmail.com>
2015-11-15 14:42:08 +0100

    Initial working versions of "WriteSingleCoil" and "WriteSingleRegister"

--- src/lib.rs
+++ src/lib.rs
@@ -7,44 +7,64 @@ extern crate byteorder;
 
 pub mod tcp;
 
-enum_from_primitive! {
-#[derive(Debug, PartialEq)]
-/// Modbus function codes
-pub enum FunctionCode {
-    ReadCoils               = 0x01,
-    ReadDiscreteInputs      = 0x02,
-    ReadHoldingRegisters    = 0x03,
-    ReadInputRegisters      = 0x04,
-    WriteSingleCoil         = 0x05,
-    WriteSingleRegister     = 0x06,
-    ReadExceptionStatus     = 0x07,
-    WriteMultipleCoils      = 0x0f,
-    WriteMultipleRegisters  = 0x10,
-    ReportSlaveId           = 0x11,
-    MaskWriteRegister       = 0x16,
-    WriteAndReadRegisters   = 0x17
+type Address  = u16;
+type Quantity = u16;
+type Value    = u16;
+
+enum Function {
+    ReadCoils(Address, Quantity),
+    ReadDiscreteInputs(Address, Quantity),
+    ReadHoldingRegisters(Address, Quantity),
+    ReadInputRegisters(Address, Quantity),
+    WriteSingleCoil(Address, Value),
+    WriteSingleRegister(Address, Value)
 }
+
+impl Function {
+    fn code(&self) -> u8 {
+        match *self {
+            Function::ReadCoils(_, _)             => 0x01,
+            Function::ReadDiscreteInputs(_, _)    => 0x02,
+            Function::ReadHoldingRegisters(_, _)  => 0x03,
+            Function::ReadInputRegisters(_, _)    => 0x04,
+            Function::WriteSingleCoil(_, _)       => 0x05,
+            Function::WriteSingleRegister(_, _)   => 0x06
+        }
+    //
+    // ReadExceptionStatus     = 0x07,
+    // WriteMultipleCoils      = 0x0f,
+    // WriteMultipleRegisters  = 0x10,
+    // ReportSlaveId           = 0x11,
+    // MaskWriteRegister       = 0x16,
+    // WriteAndReadRegisters   = 0x17
+    }
 }
+
 
 enum_from_primitive! {
 #[derive(Debug, PartialEq)]
 /// Modbus exception codes
-pub enum ExceptionCode {
-    IllegalFunction     = 0x01,
-    IllegalDataAddress,
-    IllagalDataValue,
-    SlaveOrServerFailure,
-    Acknowledge,
-    SlaveOrServerBusy,
-    NegativeAcknowledge,
-    MemoryParity,
-    NotDefined,
-    GatewayPath,
-    GatewayTarget
+pub enum ModbusExceptionCode {
+    IllegalFunction         = 0x01,
+    IllegalDataAddress      = 0x02,
+    IllegalDataValue        = 0x03,
+    SlaveOrServerFailure    = 0x04,
+    Acknowledge             = 0x05,
+    SlaveOrServerBusy       = 0x06,
+    NegativeAcknowledge     = 0x07,
+    MemoryParity            = 0x08,
+    NotDefined              = 0x09,
+    GatewayPath             = 0x0a,
+    GatewayTarget           = 0x0b
 }
 }
 
-pub type ModbusResult = std::result::Result<ExceptionCode, ExceptionCode>;
+pub enum IoError {
+    ModbusExceptionCode(ModbusExceptionCode),
+    Communication
+}
+
+pub type ModbusResult<T> = std::result::Result<T, IoError>;
 
 enum_from_primitive! {
 #[derive(Debug, PartialEq)]
--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -1,18 +1,22 @@
 use std::u16;
-use std::net::{TcpStream};
-use std::io::{Write, Read, Result};
+use std::net::{TcpStream, Shutdown};
+use std::io;
+use std::io::{Write, Read, Cursor};
 use std::time::Duration;
-use std::process::{Command, Child, Stdio};
-use std::thread;
-
-use byteorder::{BigEndian, WriteBytesExt};
+use std::borrow::BorrowMut;
+use byteorder::{BigEndian, WriteBytesExt, ReadBytesExt};
 use bincode::rustc_serialize::{encode, decode};
 use bincode::SizeLimit;
 
-use {FunctionCode, ModbusResult, ExceptionCode, BitValue};
+use enum_primitive::FromPrimitive;
 
-const PROTOCOL_MODBUS_TCP: u16 = 0x0000;
+use {Function, ModbusResult, ModbusExceptionCode, IoError, BitValue};
+
+const MODBUS_PROTOCOL_TCP: u16 = 0x0000;
 const MODBUS_TCP_DEFAULT_PORT: u16 = 502;
+const MODBUS_HEADER_SIZE: usize = 7;
+const MODBUS_MAX_READ_COUNT: usize = 0x7d;
+const MODBUS_MAX_WRITE_COUNT: usize = 0x79;
 
 /// Context object which holds state for all modbus operations.
 pub struct Ctx {
@@ -24,12 +28,12 @@ pub struct Ctx {
 impl Ctx {
     /// Create a new context `Ctx` context object and connect it to `addr` on modbus-tcp default
     /// port (502)
-    pub fn new(addr: &str) -> Result<Ctx> {
+    pub fn new(addr: &str) -> io::Result<Ctx> {
         Self::new_with_port(addr, MODBUS_TCP_DEFAULT_PORT)
     }
 
     /// Create a new context `Ctx` context object and connect it to `addr` on port `port`
-    pub fn new_with_port(addr: &str, port: u16) -> Result<Ctx> {
+    pub fn new_with_port(addr: &str, port: u16) -> io::Result<Ctx> {
         match TcpStream::connect((addr, port)) {
             Ok(s) => {
                 // set some sane tcp socket options
@@ -38,7 +42,7 @@ impl Ctx {
                 try!(s.set_write_timeout(Some(t)));
 //                try!(s.set_nodelay(true));
 //                try!(s.set_keepalive(None));
-                Ok(Ctx { tid: 0, uid: 0, stream: s })
+                Ok(Ctx { tid: 0, uid: 1, stream: s })
             }
             Err(e) => Err(e)
         }
@@ -47,7 +51,7 @@ impl Ctx {
     /// Create a new transaction Id, incrementing the previous one.
     ///
     /// The Id is wrapping around if the Id reaches `u16::MAX`.
-    fn new_tid(self: &mut Self) -> u16 {
+    fn new_tid(&mut self) -> u16 {
         // wrap around or increment
         if self.tid  < u16::MAX {
             self.tid += 1u16;
@@ -58,70 +62,122 @@ impl Ctx {
     }
 }
 
+impl Drop for Ctx {
+    fn drop(&mut self) {
+        self.stream.shutdown(Shutdown::Both).unwrap();
+    }
+}
+
 #[derive(RustcEncodable, RustcDecodable)]
 #[repr(packed)]
-struct Packet {
+struct Header {
     tid: u16,
     pid: u16,
     len: u16,
-    uid: u8,
-    fun: u8,
-    addr: u16,
+    uid: u8
 }
 
-impl Packet {
-    fn new(ctx: &mut Ctx, fun: FunctionCode, addr: u16) -> Packet {
-        Packet {
+impl Header {
+    fn new(ctx: &mut Ctx, len: u16) -> Header {
+        Header {
             tid: ctx.new_tid(),
-            pid: PROTOCOL_MODBUS_TCP,
-            len: 4u16,
-            uid: ctx.uid,
-            fun: fun as u8,
-            addr: addr,
+            pid: MODBUS_PROTOCOL_TCP,
+            len: len,
+            uid: ctx.uid
         }
     }
+}
 
-    fn encode_with_data(self: &mut Self, data: &mut Vec<u8>) -> Vec<u8>
+
+pub fn write_single_coil(ctx: &mut Ctx, addr: u16, value: BitValue) -> ModbusResult<()>
+{
+    write_single(ctx, Function::WriteSingleCoil(addr, value as u16))
+}
+
+pub fn write_single_register(ctx: &mut Ctx, addr: u16, value: u16) -> ModbusResult<()>
+{
+    write_single(ctx, Function::WriteSingleRegister(addr, value))
+}
+
+fn write_single(ctx: &mut Ctx, fun: Function) -> ModbusResult<()>
+{
+    let (addr, value) = match fun {
+        Function::WriteSingleCoil(a, v)     => (a, v),
+        Function::WriteSingleRegister(a, v) => (a, v),
+        _ => panic!("Unexpected modbus function")
+    };
+
+    let mut buff = vec![0; MODBUS_HEADER_SIZE];  // Header gets filled in later
+    buff.write_u8(fun.code()).unwrap();
+    buff.write_u16::<BigEndian>(addr).unwrap();
+    buff.write_u16::<BigEndian>(value).unwrap();
+    write(ctx, &mut buff)
+}
+
+fn write(ctx: &mut Ctx, buff: &mut Vec<u8>) -> ModbusResult<()> {
+    if buff.len() > MODBUS_MAX_WRITE_COUNT {
+        return Err(IoError::ModbusExceptionCode(ModbusExceptionCode::IllegalDataValue));
+    }
+    let header = Header::new(ctx, (buff.len() - MODBUS_HEADER_SIZE + 1) as u16);
+    let head_buff = encode(&header, SizeLimit::Infinite).unwrap();
     {
-        self.len += data.len() as u16;
-        let mut pack: Vec<u8> = encode(self, SizeLimit::Infinite).unwrap();
-        pack.append(data);
-        pack
+        let mut start = Cursor::new(buff.borrow_mut());
+        start.write(&head_buff[..]).unwrap();
     }
-}
-
-fn write_single(ctx: &mut Ctx, fun: FunctionCode, addr: u16, value: u16) -> ModbusResult
-{
-    let mut data = vec![];
-    data.write_u16::<BigEndian>(value).unwrap();
-    let pack = Packet::new(ctx, fun, addr).encode_with_data(&mut data);
-    println!("{:?}", pack);
-    match ctx.stream.write_all(&pack) {
-        Ok(_)  => {
-            let mut in_buf = Vec::new();
-            ctx.stream.read_to_end(&mut in_buf);
-            Ok(ExceptionCode::Acknowledge)
+    match ctx.stream.write_all(&buff[..]) {
+        Ok(_s) => {
+                let reply = &mut [0; 12];
+                match ctx.stream.read(reply) {
+                    Ok(_s) => {
+                        let resp_hd = decode(&reply[..MODBUS_HEADER_SIZE]).unwrap();
+                        validate_response_header(&header, &resp_hd).and(
+                            validate_response_code(&buff, reply))
+                    }
+                    Err(_e) => Err(IoError::Communication)
+                }
         }
-        Err(e) => panic!("Error sending data: {}", e)
+        Err(_e) => Err(IoError::Communication)
     }
-
 }
 
-pub fn write_single_coil(ctx: &mut Ctx, addr: u16, v: BitValue) -> ModbusResult
-{
-    write_single(ctx, FunctionCode::WriteSingleCoil, addr, v as u16)
+fn validate_response_header(req: &Header, resp: &Header) -> ModbusResult<()> {
+    if req.tid != resp.tid || resp.pid != MODBUS_PROTOCOL_TCP {
+        Err(IoError::Communication)
+    }
+    else {
+        Ok(())
+    }
+}
+
+fn validate_response_code(req: &[u8], resp: &[u8]) -> ModbusResult<()> {
+    if req[7] + 0x80  == resp[7] {
+        let code = ModbusExceptionCode::from_u8(resp[8]).unwrap();
+        Err(IoError::ModbusExceptionCode(code))
+    }
+    else if req[7] != resp[7] {
+        Err(IoError::Communication)
+    }
+    else {
+        Ok(())
+    }
 }
 
 #[cfg(test)]
-fn start_dummy_server(port: &str) -> Result<ChildKiller> {
-    Ok(ChildKiller(Command::new("./test/diagslave")
+fn start_dummy_server(port: &str) -> ChildKiller {
+    use std::process::{Command, Stdio};
+    use std::thread::sleep_ms;
+    let ck = ChildKiller(Command::new("./test/diagslave")
                         .arg("-m").arg("tcp")
                         .arg("-p").arg(port)
                         .stdout(Stdio::null())
                         .spawn()
-                        .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) })))
+                        .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) }));
+    sleep_ms(500);
+    ck
 }
 
+#[cfg(test)]
+use std::process::Child;
 #[cfg(test)]
 struct ChildKiller(Child);
 
@@ -135,21 +191,30 @@ impl Drop for ChildKiller {
 #[test]
 fn test_packet_tid_creation() {
     let _s = start_dummy_server("2222");
-    thread::sleep_ms(500);
     let mut ctx = Ctx::new_with_port("127.0.0.1", 2222).unwrap();
-    let mut hd = Packet::new(&mut ctx, FunctionCode::ReadCoils, 0);
+    let mut hd = Header::new(&mut ctx, 10);
     assert!(hd.tid == 1u16);
-    hd = Packet::new(&mut ctx, FunctionCode::ReadCoils, 0);
+    hd = Header::new(&mut ctx, 10);
     assert!(hd.tid == 2u16);
     ctx.tid = u16::MAX;
-    hd = Packet::new(&mut ctx, FunctionCode::ReadCoils, 0);
+    hd = Header::new(&mut ctx, 10);
     assert!(hd.tid == 0);
 }
 
 #[test]
 fn test_write_single_coil() {
     let _s = start_dummy_server("2223");
-    thread::sleep_ms(500);
     let mut ctx = Ctx::new_with_port("127.0.0.1", 2223).unwrap();
-    assert!(write_single_coil(&mut ctx, 0, BitValue::On).is_ok());
+    for i in 0..10 {
+        assert!(write_single_coil(&mut ctx, i, BitValue::On).is_ok());
+    }
+}
+
+#[test]
+fn test_write_single_register() {
+    let _s = start_dummy_server("2224");
+    let mut ctx = Ctx::new_with_port("127.0.0.1", 2224).unwrap();
+    for i in 0..10 {
+        assert!(write_single_register(&mut ctx, i, i).is_ok());
+    }
 }
