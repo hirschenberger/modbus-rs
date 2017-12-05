08414deb2af76704644db1fd973a246fbfbc71a5
chris ricketts <chris.ricketts@steribar.com>
2017-12-05 14:01:55 +0000

    Added an option to have a TCP connection timeout when creating a Transport. Added two tests for said changes.

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,33 @@
 extern crate test_server;
 extern crate modbus;
 
+mod connection_tests {
+    use modbus::tcp::{Config, Transport};
+    use std::time::{Duration, Instant};
+
+    #[test]
+    fn test_connect_timeout() {
+        let mut cfg = Config::default();
+        cfg.tcp_connect_timeout = Some(Duration::from_millis(1000));
+        let now = Instant::now();
+        if let Err(_) = Transport::new_with_cfg("30.30.30.30", cfg) {
+            let elapsed = now.elapsed().as_secs();
+            assert_eq!(elapsed, 1, "Elapsed: {}", elapsed);
+        }
+    }
+
+    #[test]
+    fn test_default_timeout() {
+        let cfg = Config::default();
+        let now = Instant::now();
+        if let Err(_) = Transport::new_with_cfg("30.30.30.30", cfg) {
+            let elapsed = now.elapsed().as_secs();
+            println!("Default OS TCP Timeout: {} secs", elapsed);
+            assert!(true);
+        }
+    }
+}
+
 #[cfg(feature="modbus-server-tests")]
 mod modbus_server_tests {
     use test_server::{ChildKiller, start_dummy_server};
